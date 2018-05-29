a8f58f4cc6cde260634ac229bd247e2d8972cdd1
Rincewind34 <rincewind34@wtnet.de>
2018-05-29 10:03:54 +0200

    [~] Refactored test class

---
+++ src/test/java/de/rincewind/interfaceapi/elements/util/PointIteratorTest.java
@@ -0,0 +1,100 @@
+package de.rincewind.interfaceapi.elements.util;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+import org.junit.Test;
+
+import de.rincewind.interfaceapi.gui.elements.util.Point;
+import de.rincewind.interfaceapi.gui.elements.util.PointIterator;
+import junit.framework.Assert;
+
+public class PointIteratorTest {
+	
+	private static Set<Point> newSet(Point... points) {
+		Set<Point> result = new HashSet<>();
+		result.addAll(Arrays.asList(points));
+		return result;
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testInitNegativX() {
+		new PointIterator(-1, 5);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testInitNegativY() {
+		new PointIterator(6, -1);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testInitNullWindow() {
+		new PointIterator(null);
+	}
+	
+	@Test(expected = NoSuchElementException.class)
+	public void testIterateNoElement() {
+		PointIterator incrementor = new PointIterator(Point.NULL, Point.NULL);
+		Assert.assertEquals(Point.NULL, incrementor.next());
+		incrementor.next();
+	}
+	
+	@Test
+	public void testIterateZero() {
+		Set<Point> points = PointIteratorTest.newSet(new Point(2, 1));
+		int carry = 0;
+		
+		for (Point point : new PointIterator(2, 1, 2, 1)) {
+			points.remove(point);
+			carry = carry + 1;
+		}
+		
+		Assert.assertEquals(0, points.size());
+		Assert.assertEquals(1, carry);
+	}
+	
+	@Test
+	public void testIterateX() {
+		Set<Point> points = PointIteratorTest.newSet(new Point(-1, 1), new Point(0, 1), new Point(1, 1));
+		int carry = 0;
+		
+		for (Point point : new PointIterator(-1, 1, 1, 1)) {
+			points.remove(point);
+			carry = carry + 1;
+		}
+		
+		Assert.assertEquals(0, points.size());
+		Assert.assertEquals(3, carry);
+	}
+	
+	@Test
+	public void testIterateY() {
+		Set<Point> points = PointIteratorTest.newSet(new Point(0, -3), new Point(0, -2), new Point(0, -1));
+		int carry = 0;
+		
+		for (Point point : new PointIterator(0, -3, 0, -1)) {
+			points.remove(point);
+			carry = carry + 1;
+		}
+		
+		Assert.assertEquals(0, points.size());
+		Assert.assertEquals(3, carry);
+	}
+	
+	@Test
+	public void testIterateSquare() {
+		Set<Point> points = PointIteratorTest.newSet(new Point(-1, -3), new Point(-1, -2), new Point(0, -3), new Point(0, -2));
+		int carry = 0;
+		
+		for (Point point : new PointIterator(-1, -3, 0, -2)) {
+			points.remove(point);
+			carry = carry + 1;
+		}
+		
+		Assert.assertEquals(0, points.size());
+		Assert.assertEquals(4, carry);
+	}
+	
+}
