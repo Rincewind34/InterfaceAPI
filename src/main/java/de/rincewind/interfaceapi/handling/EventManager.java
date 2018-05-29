b6e971680d4ac85c252232ec13df4bf130d82f75
Rincewind34 <rincewind34@wtnet.de>
2018-05-29 10:14:11 +0200

    [+] Added some java docs

--- src/main/java/de/rincewind/interfaceapi/handling/EventManager.java
+++ src/main/java/de/rincewind/interfaceapi/handling/EventManager.java
@@ -1,16 +1,15 @@
 package de.rincewind.interfaceapi.handling;
 
 import de.rincewind.interfaceapi.gui.components.EventBased;
-import de.rincewind.interfaceapi.gui.elements.abstracts.Element;
-import de.rincewind.interfaceapi.gui.windows.abstracts.Window;
 
 /**
- * This class handles the events and listeners to some Objects like {@link Element} or
- * {@link Window}.
- * 
- * There are many events could be fired and each of them has one attendant listener-class
- * ({@link InterfaceListener}).
- * If an event is fired all registered listeners, handling the fired event, will be automatically executed.
+ * This class handles the events and listeners to some objects extending the
+ * interface {@link EventBased}.<br>
+ * <br>
+ * There are many events could be fired and each of them has one attendant
+ * listener-class ({@link InterfaceListener}). If an event is fired all
+ * registered listeners, handling the fired event, will be automatically
+ * executed.
  * 
  * @author Rincewind34
  * @since 2.3.3
@@ -18,25 +17,28 @@ import de.rincewind.interfaceapi.gui.windows.abstracts.Window;
  * @see EventBased
  */
 public interface EventManager {
-	
+
 	public abstract int countRegisteredListeners(Class<? extends Event> eventCls);
-	
+
 	public abstract int countRegisteredMonitorListeners(Class<? extends Event> eventCls);
-	
+
 	public abstract int countRegisteredPipelineListeners(Class<? extends Event> eventCls);
-	
+
 	public abstract int countActiveListeners(Class<? extends Event> eventCls);
-	
+
 	public abstract <E extends Event> void callEvent(Class<E> eventCls, E event);
-	
+
 	public abstract <E extends Event> ListenerBase registerListener(Class<E> eventCls, InterfaceListener<E> listener);
-	
+
 	/**
-	 * This class is a dummy to let the user decide if the listener to register has to
-	 * be executed before or after all even registered listeners.
-	 * To finally register the listener use {@link ListenerBase#addAfter()} and
-	 * {@link ListenerBase#addBefore()}. You have to execute one of these methods to
-	 * register the listener. Otherwise the listener will never be executed!
+	 * This class is a dummy to let the user decide if the listener to register
+	 * has to be executed before or after all already registered listeners. To
+	 * finally register the listener use {@link #addAfter()},
+	 * {@link #addBefore()} or {@link #monitor()}. You have to execute one of
+	 * these methods to finally register the listener. Otherwise the listener
+	 * will never be called!<br>
+	 * <br>
+	 * More information for event pipeline: {@link EventManager}
 	 * 
 	 * @author Rincewind34
 	 * @since 2.3.3
@@ -44,14 +46,35 @@ public interface EventManager {
 	 * @see EventManager#registerListener(Class, InterfaceListener)
 	 */
 	public static interface ListenerBase {
-		
+
+		/**
+		 * Adds the concerning listener after all working listeners already
+		 * registered in the pipeline.<br>
+		 * <br>
+		 * More information for event pipeline: {@link EventManager}
+		 */
 		public abstract void addAfter();
-		
+
+		/**
+		 * Adds the concerning listener before all working listeners already
+		 * registered in the pipeline.<br>
+		 * <br>
+		 * More information for event pipeline: {@link EventManager}
+		 */
 		public abstract void addBefore();
-		
+
+		/**
+		 * Registers the concerning listener to monitor the if the event is
+		 * called. The listener is not allowed to change any values (including
+		 * cancel the event if supported) while monitoring the event. <br>
+		 * The registered listeners monitoring the event are called after all
+		 * working listeners. The order the monitor listeners are called is
+		 * undefined.<br>
+		 * <br>
+		 * More information for event pipeline: {@link EventManager}
+		 */
 		public abstract void monitor();
-		
+
 	}
 
-	
 }
